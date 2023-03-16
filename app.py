0c223e6998208cef4c00b2076db105572410756e
Kunal Tambe <107045326+kt0415@users.noreply.github.com>
2023-03-16 06:08:09 +0000

    Modified the home.html file, used special templates to include jobs in the home page of the website.

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'salary': 'Rs. 10,00,000',
+  'location': 'Bangalore, India'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'salary': 'Rs. 15,00,000',
+  'location': 'Delhi, India'
+}, {
+  'id': 3,
+  'title': 'Frontend Intern',
+  'stipend': 'Rs. 20000/month',
+  'location': 'Remote'
+}, {
+  'id': 4,
+  'title': 'Software Developer',
+  'salary': '200000 AUD',
+  'location': 'Canberra, Australia'
+}]
+
+
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
-if(__name__) == "__main__":
+
+if (__name__) == "__main__":
   app.run(host='0.0.0.0', debug=True)
-  
